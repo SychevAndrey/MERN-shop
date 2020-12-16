310bd9f9956203c92c984cff65c42ad62f5345b2
Andrey Sychev <nukemoore@gmail.com>
2020-12-16 10:24:27 +0300

    authorization and protecting content

--- utils/auth.js
+++ utils/auth.js
@@ -3,5 +3,21 @@ import Router from 'next/router';
 
 export function handleLogin(token) {
     cookie.set('token', token);
+    localStorage.setItem('login', Date.now());
     Router.push('/account');
+}
+
+export function handleLogout() {
+    cookie.remove('token');
+    localStorage.setItem('logout', Date.now());
+    Router.push('/login');
+}
+
+export function redirectUser(ctx, location) {
+    if (ctx.req) {
+        ctx.res.writeHead(302, { Location: location});
+        ctx.res.end();
+    } else {
+        Router.push(location);
+    }
 }
